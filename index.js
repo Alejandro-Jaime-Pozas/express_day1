5bf13a1cb64115c0272e86359ca99af72b4d6b71
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-09-06 14:42:22 -0500

    added logger fn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const port = 3000; // the port it will use for http
 
 app.set('view engine', 'ejs') // this sets the default engine to ejs embedded js templates
 
+app.use(logger) // this means every req after line 7 will first use the logger middelware; check the logger function below for output
+
 app.get('/', (req, res) => { // like the GET method for API, url first, req is like the request in postman, res is the response. 1st arg always req, 2nd the res
+    console.log(req.test) // calling req.test from the logger fn below
     res.render('index', {firstName: 'Alex', lastName: 'Jaime'}); // this is how we would send a response, first the file 'index', then the object {k:v}
 })
 
@@ -41,6 +44,7 @@ app.get('/users/:id', (req, res) => { // this gets the url made by the client/us
     for (let user of users){
         if (user.id == id){ // two equal signs since id is a string...
             res.render('user', { user }) // { user } is now a local, and can be used in ejs file
+            
         }
     }
     res.send({error: `User with id ${id} does not exist`})
@@ -51,9 +55,14 @@ app.get('/test/:testId/:abc/:xyz', (req, res) => {
     res.send('Test')
 })
 
+function logger(req, res, next){
+    console.log(req.originalUrl) // this gives us the URL of what we are doing
+    req.test = 123 // here we are creating a .test property on the req
+    next();
+}
+
 
 // this 'listens' for connections
 app.listen(port, () => {
     console.log(`hello world app listening on port ${port}`)
 });
-
